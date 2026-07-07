0a0f9750a2fcd936c5f95ccd2c051327dc1640bb
dandmb <danbizwa@gmail.com>
2026-07-08 01:26:29 +0200

    email template implementation done

--- notification/build.gradle.kts
+++ notification/build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
     implementation(projects.common)
     testImplementation(kotlin("test"))
     implementation(libs.spring.boot.starter.amqp)
+    implementation(libs.spring.boot.starter.thymeleaf)
 }
 
 kotlin {
